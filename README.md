eb6a252506f3eb755a23ab914ada0febd5af3b2d
yannisme <yannisme@hotmail.com>
2021-07-01 01:09:10 +0800

    update 1.0.0

---
+++ README.md
@@ -0,0 +1,26 @@
+# flarum-confix-theme
+flarum-confix-theme
+
+### 演示
+
+[点击查看](https://flarum.pro/)
+
+### 安装
+
+使用 composer 命令进行安装:
+
+```sh
+composer require yannisme/confixtheme
+```
+
+### 更新
+
+```sh
+composer update yannisme/confixtheme
+php flarum cache:clear
+```
+
+### 链接
+
+- [Github](https://github.com/yannisme/flarum-confix-theme)
+- [Packagist](https://packagist.org/packages/yannisme/confixtheme)
---
+++ extend.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of yannis/confixtheme.
+ *
+ * Copyright (c) 2021 yannis.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Yannis\ConfixTheme;
+
+use Flarum\Extend;
+
+return [
+    (new Extend\Frontend('forum'))
+        
+        ->css(__DIR__.'/resources/less/forum.less'),
+    (new Extend\Frontend('admin'))
+        
+        ->css(__DIR__.'/resources/less/admin.less'),
+    new Extend\Locales(__DIR__ . '/resources/locale')
+];
